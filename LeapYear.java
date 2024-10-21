135f87730f1143d31b8a32cac6740787136204cf
Bat-Orgil Erdenebat <berdeneb@go.olemiss.edu>
2024-10-20 23:56:06 -0500

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,14 @@
+import java.util.Scanner ;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in) ;
+		int year ;
+		System.out.print("Enter a year") ;
+		year = input.nextInt()
+		if (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
+			System.out.printf("%d is a leap year" + year);
+else 
+System.out.printf("%d is not a leap year" + year);
+	}
+}
