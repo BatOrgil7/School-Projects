4173e65f38f2a5cd71229b84260bbeb0e07a83cc
Bat-Orgil Erdenebat <berdeneb@go.olemiss.edu>
2024-10-24 00:31:35 -0500

    Add files via upload

---
+++ MajorStatus.java
@@ -0,0 +1,33 @@
+import java.util.Scanner ;
+public class MajorStatus {
+	 public static void main(String[] args) {
+		 String majorandStatus ;
+		 String major = "", status = "";
+		 char majorC, statusC;
+		 
+		 Scanner input = new Scanner(System.in) ;
+		 System.out.print("Enter two character") ;
+		 majorAndStatus = input.nextLine();
+		 majorC = majorAndStatus.charAr(0);
+		 StatusC = majorAndStatus.charAr(1);
+		 
+		 if((majorC == ' '|| majorC == ' ' majorC == ' ') 
+			 switch (majorC) {
+				 case 'M' ;
+				 major = "Mathematics" ;
+				 break;
+				 case 'C' ;
+				 major = "ComputerScience" ;
+				 break;
+				 case 'I' ;
+				 major = "InformationScience" ;
+				 break;
+				 
+			 switch (StatusC) {
+              				 
+				 
+
+
+		 else(
+		 System.out.println("Invalid input") ;
+		 
