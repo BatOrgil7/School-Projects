89a3023865b4df9e71104c06c3ae248f82d6f28c
Bat-Orgil Erdenebat <berdeneb@go.olemiss.edu>
2024-10-25 23:37:03 -0500

    Add files via upload

---
+++ Multiple .java
@@ -0,0 +1,19 @@
+import java.util.Scanner ; 
+
+public class Multiple {
+	public static void main(String[] args) {
+		int sum ;
+		
+		Scanner input = new Scanner(System.in) ;
+		System.out.print("Enter an integer: ");
+		num = input.next.Int();
+		
+		if(num % 5 == 0) {
+			System.out.println("Highfive");
+		}
+		
+		if(num % 2 == 0) {
+			System.out.println("hiEven");
+		}
+	}
+}
