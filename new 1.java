158b0b8692c4ca7adc881c4aa564d7c6faebcbea
Bat-Orgil Erdenebat <berdeneb@go.olemiss.edu>
2024-10-28 20:29:52 -0500

    Add files via upload

---
+++ new 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Test(
+   public class void main(String[] args){
+      //declare variables
+	  int num1, num2;
+	  double avarage;
+	  
+	  //ask the user to enter two integers
+	  Scanner input = new Scanner(Systemn.in);
+	  num1 = input.nextint();
+	  System.out.print("Enter second integer: ") ;
+	  num2 = input.nextInt();
+	  
+	  //calculate avarage
+	  avarage = (num1 + num2) / 2.0;
+	  //display avarage
+	  System.out.println("The avarage is: " + avarage);
+	  System.out.printf("The avarage is 
