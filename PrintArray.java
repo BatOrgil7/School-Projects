22e9523a9f7ceb4cd8efc2fd38358ec9ccca81a0
Bat-Orgil Erdenebat <berdeneb@go.olemiss.edu>
2024-10-29 09:07:08 -0500

    Add files via upload

---
+++ PrintArray.java
@@ -0,0 +1,11 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        // Assuming you have an array named integers
+        int[] integers = {1, 2, 3, 4, 5};
+
+        // Using a for-each loop to print each element
+        for (int value : integers) {
+            System.out.println(value);
+        }
+    }
+}
